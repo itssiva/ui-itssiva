eb9bf58471c845d66d1bf752708c290c413291b4
Siva Ponugoti <siva@yewno.com>
2016-11-20 21:36:03 -0800

    update files to make it bare react

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {ToDoList} from './container';
-import {List, Map} from 'immutable';
-import {createStore} from 'redux';
-import reducer from './reducer';
-import {Provider} from 'react-redux';
 
-const store = createStore(reducer);
-
-// const dummyTodos = List([
-//   Map({ id: 0, isDone: true,  text: 'make components' }),
-//   Map({ id: 1, isDone: false, text: 'design actions' }),
-//   Map({ id: 2, isDone: false, text: 'implement reducer' }),
-//   Map({ id: 3, isDone: false, text: 'connect components' })
-// ]);
+function Component(props) {
+  return <div>Hello World!</div>
+}
 
 render(
-  <Provider store={store}>
-    <ToDoList />
-  </Provider>,
+  <Component />
   document.getElementById('app')
 );
