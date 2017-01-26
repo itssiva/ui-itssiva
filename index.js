e1e29336ddf58d47631ac9b7a83c16515d4d4223
Siva <siva@yewno.com>
2017-01-25 22:48:54 -0800

    added education to portfolio

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Header from './components/header/'
+import Header from './components/header/';
+import Education from './components/education/'
 
+let educationDegrees = [
+  {
+    level: "Masters",
+    major: "Computer Science",
+    schoolName: "University of North Carolina at Charlotte",
+    location: "NC, USA",
+    startYear: 2014,
+    endYear: 2015,
+  },
+  {
+    level: "Bachelor of Technology",
+    major: "Information Technology",
+    schoolName: "Andhra University",
+    location: "Visakhapatnam, AP, India",
+    startYear: 2010,
+    endYear: 2014,
+  },
+]
 
 render(
   (<div>
-    <Header/>
+    <Header />
+    <Education educationDegrees={educationDegrees}/>
+
   </div>),
   document.getElementById('app')
 );
